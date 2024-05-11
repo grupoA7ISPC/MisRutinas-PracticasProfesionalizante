22a4d021f67334ff017158b15b2f6dc0e8b1b833
Carina Payleman <rociocarina2@gmail.com>
2024-05-11 15:59:58 -0300

    Adapta UI products cards y container, elimina archivo products.html

--- Web/frontend/src/app/pages/products/products.component.ts
+++ Web/frontend/src/app/pages/products/products.component.ts
@@ -4,7 +4,20 @@ import { ClasesService } from 'src/app/services/clases.service';
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html',
+  template: `<div class="container">
+  <!-- <div *ngFor="let suscripcion of suscripciones" class="box basic">
+      <h2>{{suscripcion.nombre}}</h2>
+      <p>{{ suscripcion.descripcion }}<p>
+      <p>{{suscripcion.dur_dias}} dias</p>
+      <p>Precio: {{ suscripcion.price |currency }}</p>
+      <a routerLink="/pago" class="subscribe-button">Suscribirse</a>
+  </div> -->
+  <app-card-prod></app-card-prod>
+  <app-card-prod></app-card-prod>
+  <app-card-prod></app-card-prod>
+  <app-card-prod></app-card-prod>
+  <app-card-prod></app-card-prod>
+  </div>`,
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
@@ -15,7 +28,7 @@ export class ProductsComponent {
     {
       id: 1,
       nombre: "Suscripcion Básica",
-      descripcion: 
+      descripcion:
         "Acceso a funcionalidades básicas de nuestra plataforma"
       ,
       dur_dias: "Membresia Mensual",
@@ -24,17 +37,17 @@ export class ProductsComponent {
     {
       id: 2,
       nombre: "Premium Mensual",
-      descripcion: 
+      descripcion:
         "Acceso completo a nuestra plataforma",
       dur_dias: "Membresia Mensual",
       price: 1000
     }
   ]
 //   ngOnInit() {
-//     this.clasesService.getAllSubscriptions() 
+//     this.clasesService.getAllSubscriptions()
 //     .subscribe(data => {
 //     console.log(data);
-//      this.suscripciones = data //Cambiar para renderizado luego. 
+//      this.suscripciones = data //Cambiar para renderizado luego.
 //   });
 //  }
 
