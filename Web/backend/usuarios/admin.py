f0b19c8f167618ca641dd4f388ff9a075c4a79a3
fede9087 <fede9087@gmail.com>
2024-05-12 00:00:42 -0300

    Creo las tablas de user y rol

--- Web/backend/usuarios/admin.py
+++ Web/backend/usuarios/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class RolAdmin (admin.ModelAdmin):
+    list_display = ('id_rol','nombre_rol')
+    
+
+class UsuarioAdmin (admin.ModelAdmin):
+    list_display = ('id_user','id_rol','nombre','apellido','fec_nac','email','tel')
+
+admin.site.register(Rol, RolAdmin)
+admin.site.register(Usuario, UsuarioAdmin)
--- Web/backend/usuarios/models.py
+++ Web/backend/usuarios/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Rol(models.Model):
+    id_rol  = models.AutoField(primary_key=True)
+    nombre_rol = models.CharField(max_length=5) #Admin-Socio
+
+    class Meta:
+        db_table = "Rol"
+        verbose_name = "Rol de usuario"
+        verbose_name_plural = "Roles"
+        
+    def __unicode__(self):
+        return self.nombre_rol
+    
+    def __str__(self):
+        return self.nombre_rol
+    
+class Usuario(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    id_rol = models.ForeignKey(Rol,on_delete=models.CASCADE,default=2) # 2=Socio
+    nombre = models.CharField(max_length=15)
+    apellido = models.CharField(max_length=15)
+    fec_nac = models.DateField()
+    email = models.EmailField(max_length=150)
+    password = models.CharField(max_length=16)
+    dni = models.IntegerField()
+    tel = models.CharField(max_length=15)
+    
+    class Meta:
+        db_table="Usuario"
+        verbose_name="Usuario de MisRutinas"
+        verbose_name_plural="Usuarios"
+
+    # NOMBRE + APELLIDO
+    def __unicode__(self):
+        return self.nombre + " " + self.apellido
+    
+    # NOMBRE + APELLIDO
+    def __str__(self):
+        return self.nombre + " " + self.apellido
