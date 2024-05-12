da65e9de11d46b8d86b8d06545eca80eca743220
fede9087 <fede9087@gmail.com>
2024-05-12 01:19:53 -0300

    Creo las tablas de Clase, UsuarioxClase, Factura, pago

--- Web/backend/clases/admin.py
+++ Web/backend/clases/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class PagoAdmin(admin.ModelAdmin):
+    list_display = ('id_pago','fecha')
+    
+class ClaseAdmin(admin.ModelAdmin):
+    list_display = ('id_clase','nombre','precio','descripcion','duracion')
+    
+class UsuarioClaseAdmin(admin.ModelAdmin):
+    list_display = ('id_user_clase','id_clase','id_user')
+
+class FacturaAdmin(admin.ModelAdmin):
+    list_display = ('id_factura','total','fecha','id_user_clase','id_pago')
+
+admin.site.register(Pago, PagoAdmin)
+admin.site.register(Clase, ClaseAdmin)
+admin.site.register(UsuarioClase, UsuarioClaseAdmin)
+admin.site.register(Factura, FacturaAdmin)
--- Web/backend/clases/models.py
+++ Web/backend/clases/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from usuarios.models import Usuario
 
-# Create your models here.
+
+class Pago(models.Model):
+    id_pago = models.AutoField(primary_key=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        db_table = "Pago"
+        verbose_name = "Pago de clase"
+        verbose_name_plural = "Pagos de clases"
+        
+    def __unicode__(self):
+        return self.id_pago 
+    
+    def __int__(self):
+        return self.id_pago
+    
+class Clase(models.Model):
+    id_clase = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=45)
+    precio = models.DecimalField(decimal_places=2, max_digits=8)
+    descripcion = models.TextField(max_length=400)
+    #imagen = models.TipopilotField
+    duracion = models.CharField(max_length=150)
+    
+    class Meta:
+        db_table = "Clase"
+        verbose_name = "Clase que da el GYM"
+        verbose_name_plural = "Clases"
+        
+    def __unicode__(self):
+        return self.nombre
+    
+    def __str__(self):
+        return self.nombre
+    
+class UsuarioClase(models.Model):
+    id_user_clase = models.AutoField(primary_key=True)
+    id_clase = models.ForeignKey(Clase,on_delete=models.CASCADE)
+    id_user = models.ForeignKey(Usuario, on_delete=models.CASCADE)  
+    
+    class Meta:
+        db_table = "UsuarioClase"
+        verbose_name = "Clase del usuario"
+        verbose_name_plural = "UsuariosClases"
+        
+    def __unicode__(self):
+        return self.id_user_clase
+    def __int__(self):
+        return self.id_user_clase
+    
+class Factura(models.Model):
+    id_factura = models.AutoField(primary_key=True)
+    total = models.DecimalField(decimal_places=2, max_digits=8)
+    fecha = models.DateTimeField(auto_now_add=True)
+    id_user_clase= models.ForeignKey(UsuarioClase,on_delete=models.CASCADE)
+    id_pago = models.ForeignKey(Pago, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = "Factura"
+        verbose_name = "Factura del pago de clase"
+        verbose_name_plural = "Facturas"
+        
+    def __unicode__(self):
+        return self.id_factura
+    def __int__(self):
+        return self.id_factura
