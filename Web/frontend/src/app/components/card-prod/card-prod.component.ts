932d8009eb60aab470888eaa68d7601c8c2b5162
estebanmartins <estebanmartins25@gmail.com>
2024-05-13 19:52:37 -0300

    Se implementa el servicio de clases para renderizar los productos en las cards

--- Web/frontend/src/app/components/card-prod/card-prod.component.ts
+++ Web/frontend/src/app/components/card-prod/card-prod.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { Clase } from 'src/app/models/clase';
 
 @Component({
   selector: 'app-card-prod',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-prod.component.css']
 })
 export class CardProdComponent {
-
+  @Input()
+  clase:Clase = {
+    id_clase:0,
+    nombre:"",
+    descripcion:"",
+    imagen:"",
+    duracion:"",
+    precio:0
+  }
 }
--- Web/frontend/src/app/pages/products/products.component.ts
+++ Web/frontend/src/app/pages/products/products.component.ts
@@ -5,18 +5,10 @@ import { ClasesService } from 'src/app/services/clases.service';
 @Component({
   selector: 'app-products',
   template: `<div class="container">
-  <!-- <div *ngFor="let suscripcion of suscripciones" class="box basic">
-      <h2>{{suscripcion.nombre}}</h2>
-      <p>{{ suscripcion.descripcion }}<p>
-      <p>{{suscripcion.dur_dias}} dias</p>
-      <p>Precio: {{ suscripcion.price |currency }}</p>
-      <a routerLink="/pago" class="subscribe-button">Suscribirse</a>
-  </div> -->
-  <app-card-prod></app-card-prod>
-  <app-card-prod></app-card-prod>
-  <app-card-prod></app-card-prod>
-  <app-card-prod></app-card-prod>
-  <app-card-prod></app-card-prod>
+  <div *ngFor="let clase of clases">
+    <app-card-prod  [clase]="clase"></app-card-prod>
+  </div>
+
   </div>`,
   styleUrls: ['./products.component.css']
 })
@@ -25,12 +17,11 @@ export class ProductsComponent {
   constructor (private clasesService: ClasesService) { }
   clases: Clase[] = []
 
-//   ngOnInit() {
-//     this.clasesService.getAllClasses()
-//     .subscribe(data => {
-//     console.log(data);
-//      this.clases = data //Cambiar para renderizado luego.
-//   });
-//  }
+  ngOnInit() {
+    this.clasesService.getAllClasses().subscribe(data => {
+    this.clases = data
+    console.log(this.clases); //Cambiar para renderizado luego.
+    });
+  }
 
 }
