cc11c0b7dfba5d90dd0375a28c85e8119d998c11
fede9087 <fede9087@gmail.com>
2024-05-17 19:07:20 -0300

    se crea el endpoint de clase

--- Web/backend/clases/views.py
+++ Web/backend/clases/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Clase, UsuarioClase, Pago
+from .serializers import ClaseSerializer, UsuarioClaseSerializer, PagoSerializer
 
-# Create your views here.
+class ClaseViewSet(viewsets.ModelViewSet):
+    queryset = Clase.objects.all()
+    serializer_class = ClaseSerializer
+
+class UsuarioClaseViewSet(viewsets.ModelViewSet):
+    queryset = UsuarioClase.objects.all()
+    serializer_class = UsuarioClaseSerializer
+
+class PagoViewSet(viewsets.ModelViewSet):
+    queryset = Pago.objects.all()
+    serializer_class = PagoSerializer
--- Web/backend/misRutinas/urls.py
+++ Web/backend/misRutinas/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/login/', include('usuarios.urls'))
+    path('api/v1/login/', include('usuarios.urls')),
+    path('api/v1/clases/', include('clases.urls')),
 ]
