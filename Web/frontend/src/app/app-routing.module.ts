1d5d0f643c0203444210b6a0310ecc57fea76f1b
Carina Payleman <rociocarina2@gmail.com>
2024-05-17 18:05:36 -0300

    Crea guard para proteger ruta /dashboard

--- Web/frontend/src/app/app-routing.module.ts
+++ Web/frontend/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent, ContactoComponent, DashboardComponent, LoginComponent, ProductsComponent, QuienesSomosComponent, RegistroComponent } from './pages';
 import { FormClaseComponent } from './pages/dashboard/form-clase/form-clase.component';
+import { AuthGuard } from './guards/auth.guard';
+
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -10,7 +12,7 @@ const routes: Routes = [
   {path: 'registro', component: RegistroComponent},
   {path: 'quienes-somos', component: QuienesSomosComponent},
   {path: 'planes', component: ProductsComponent},
-  {path: 'dashboard', component: DashboardComponent}, 
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'crear-clase', component: FormClaseComponent}
 ];
 
--- Web/frontend/src/app/layout/navbar/navbar.component.ts
+++ Web/frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,7 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 export class NavbarComponent {
   isAuthenticated: boolean = false;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.authService.estarAutenticado.subscribe((isLoggedIn: boolean) => {
       this.isAuthenticated = isLoggedIn;
     });
@@ -17,5 +18,6 @@ export class NavbarComponent {
 
   logout(): void {
     this.authService.logout();
+    this.router.navigate(['/home']);
   }
 }
