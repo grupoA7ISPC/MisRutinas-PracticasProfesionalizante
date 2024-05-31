ad2403266ebfb94d9c3cefba6caa089818746261
fede9087 <fede9087@gmail.com>
2024-05-30 22:59:05 -0300

    metodo update completo

--- Web/frontend/src/app/app-routing.module.ts
+++ Web/frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent, ContactoComponent, DashboardComponent, LoginComponent, ProductsComponent, QuienesSomosComponent, RegistroComponent } from './pages';
 import { FormClaseComponent } from './pages/dashboard/form-clase/form-clase.component';
 import { AuthGuard } from './guards/auth.guard';
+import { EditarClaseComponent } from './pages/dashboard/editar-clase/editar-clase.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path: 'quienes-somos', component: QuienesSomosComponent},
   {path: 'planes', component: ProductsComponent},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
-  {path: 'crear-clase', component: FormClaseComponent}
+  {path: 'crear-clase', component: FormClaseComponent},
+  { path: 'editar-clase/:id', component: EditarClaseComponent }
 ];
 
 @NgModule({
