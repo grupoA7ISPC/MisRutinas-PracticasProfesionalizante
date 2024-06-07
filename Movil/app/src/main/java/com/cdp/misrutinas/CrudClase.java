c6a4b832d428800a4c5c942fcafd106f9fa0054e
HoracioeQuiroga <horacioequiroga2020@gmail.com>
2024-06-07 18:32:28 -0300

    se  empiza a implementar validaciones crud clase

--- Movil/app/src/main/java/com/cdp/misrutinas/CrudClase.java
+++ Movil/app/src/main/java/com/cdp/misrutinas/CrudClase.java
@@ -4,6 +4,8 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.cdp.misrutinas.entidades.Clases;
 
@@ -17,19 +19,77 @@ public class CrudClase extends MRSQLiteHelper{
         super(contexto);
         this.context = contexto;
     }
+    private boolean areFieldsValid(FieldLengthValidation... fields) {
+        for (FieldLengthValidation fieldValidation : fields) {
+            String field = fieldValidation.field;
+            int minLength = fieldValidation.minLength;
+            int maxLength = fieldValidation.maxLength;
+
+            if (field != null && (field.length() < minLength || field.length() > maxLength)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean esValida(String nombre, String descripcion, String precio) {
+        SQLiteDatabase db = super.getWritableDatabase();
+        if (TextUtils.isEmpty(nombre) || TextUtils.isEmpty(descripcion) || TextUtils.isEmpty(precio)) {
+            Toast.makeText(context, "Todos los campos son obligatorios.", Toast.LENGTH_SHORT).show();
+            db.close();
+            return false;
+        }
+        if (!areFieldsValid(
+                new FieldLengthValidation(nombre, 1, 45),
+                new FieldLengthValidation(descripcion, 1, 200),
+                new FieldLengthValidation(precio, 1, 12)
+        )){
+            if (nombre.length() < 1 || nombre.length() > 45) {
+                Toast.makeText(context, "El nombre de la clase debe tener entre 1 y 45 caracteres.", Toast.LENGTH_SHORT).show();
+            }
+            if (descripcion.length() < 1 || descripcion.length() > 200) {
+                Toast.makeText(context, "La descripción de la clase debe tener entre 1 y 200 caracteres.", Toast.LENGTH_SHORT).show();
+            }
+            if (precio.length() < 1 || precio.length() > 12) {
+                Toast.makeText(context, "Maximo 12 números.", Toast.LENGTH_SHORT).show();
+            }
+            db.close();
+            return false;
+        }
+
+
+        return true;
+    }
     public long insertarClase (String nombreClase,String precioClase, String descripcionClase){
 
         MRSQLiteHelper usdbh = new MRSQLiteHelper(context);
         SQLiteDatabase db = usdbh.getWritableDatabase();
 
-        ContentValues values = new ContentValues();
-        values.put("nombre", nombreClase);
-        values.put("precio", precioClase);
-        values.put("descripcion", descripcionClase);
+        if (esValida(nombreClase, descripcionClase, precioClase)) {
+            try{
+                ContentValues values = new ContentValues();
+                values.put("nombre", nombreClase);
+                values.put("precio", precioClase);
+                values.put("descripcion", descripcionClase);
 
-        long id = db.insert("Clase", null, values);
+                long id = db.insert("Clase", null, values);
+                if (id != -1){
+                    db.close();
+                    return id;
 
-        return id;
+                }else {
+                    db.close();
+                    return -1;
+                }
+
+            } catch (Exception e){
+                Toast.makeText(context, "Error al insertar el registro.", Toast.LENGTH_SHORT).show();
+                db.close();
+                return -1;
+            }
+        } else {
+            db.close();
+            return -1;
+        }
 
     }
 
